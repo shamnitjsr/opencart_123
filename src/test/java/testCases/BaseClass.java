076176ad05252203c3028d0a33193a30989be185
shamnitjsr <shamnitjsr@gmail.com>
2024-09-24 08:40:04 +0530

    registration page by jenkin using master.xml

---
+++ src/test/java/testCases/BaseClass.java
@@ -0,0 +1,48 @@
+package testCases;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	
+	@BeforeClass
+	void setup() {
+
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://naveenautomationlabs.com/opencart/");
+		driver.manage().window().maximize();
+
+	}
+
+	@AfterClass
+	void tearDown() {
+
+		driver.quit();
+	}
+
+
+	public String randomString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		return generatedString;
+	}
+
+	public String randomNumber() {
+		String generatedNumber = RandomStringUtils.randomNumeric(10);
+		return generatedNumber;
+	}
+
+	public String randomAlphaNumeric() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		String generatedNumber = RandomStringUtils.randomNumeric(5);
+		return generatedString + "@" + generatedNumber;
+	}
+}
--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -1,5 +1,65 @@
 package testCases;
 
-public class TC001_AccountRegistrationTest {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+
+public class TC001_AccountRegistrationTest extends BaseClass {
+
+	
+
+//	@Test
+//	void verify_account_registration() {
+//
+//		HomePage hp = new HomePage(driver);
+//		hp.clickMyAccount();
+//		hp.clickRegister();
+//		
+//		AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
+//		
+//		regpage.setFirstName("firstName");
+//		regpage.setLastName("lastName");
+//		regpage.setEmail("qwa@gmail.com");
+//		regpage.setTelephone("1234567890");
+//		regpage.setPassword("123@qaws");
+//		regpage.setConfirmPassword("123@qaws");
+//		
+//		regpage.setPrivacyPolicy();
+//		regpage.clickContinue();
+//		
+//		
+//	}
+
+	@Test
+	void verify_account_registration() {
+
+		HomePage hp = new HomePage(driver);
+		hp.clickMyAccount();
+		hp.clickRegister();
+
+		AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
+
+		regpage.setFirstName(randomString().toUpperCase());
+		regpage.setLastName(randomString().toUpperCase());
+
+		regpage.setEmail(randomString() + "@gmail.com"); // randomly generated the email
+
+		regpage.setTelephone(randomNumber());
+
+		String password = randomAlphaNumeric();
+
+		regpage.setPassword(password);
+		regpage.setConfirmPassword(password);
+
+		regpage.setPrivacyPolicy();
+		regpage.clickContinue();
+
+		String confmsg = regpage.getConfirmationMsg();
+		Assert.assertEquals(confmsg, "Your Account Has Been Created!");
+
+	}
 
 }
